eafd0dfb30ec209ad4b9404eeff043944361a4f4
Hleb Bandarenka <glebon999@yandex.ru>
2015-09-02 21:01:09 +0300

    Lecture 1.3 - Upgrade to a new Version of Freemarker

--- src/main/java/templater/PageGenerator.java
+++ src/main/java/templater/PageGenerator.java
@@ -1,22 +1,32 @@
 package templater;
 
+import freemarker.cache.FileTemplateLoader;
+import freemarker.cache.TemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Map;
 
 public class PageGenerator {
     public static final String HTML_DIR = "templates";
-//    public static final Configuration CFG = new Configuration(Configuration.VERSION_2_3_22);
-    public static final Configuration CFG = new Configuration();
+    public static final Configuration CFG = new Configuration(Configuration.VERSION_2_3_22);
+//    public static final Configuration CFG = new Configuration();
 
-    public static String getPage(String filename, Map<String, Object> data) {
+    private static void setConfiguration() throws IOException {
+        File dir = new File(HTML_DIR);
+        TemplateLoader templateLoader = new FileTemplateLoader(dir);
+        CFG.setTemplateLoader(templateLoader);
+    }
+
+    public static String getPage(String filename, Map<String, Object> data) throws IOException {
+        setConfiguration();
         Writer stream = new StringWriter();
         try {
-            Template template = CFG.getTemplate(HTML_DIR + File.separator + filename + ".ftl");
+            Template template = CFG.getTemplate(filename + ".ftl");
             template.process(data, stream);
         } catch (Exception e) {
             e.printStackTrace();
